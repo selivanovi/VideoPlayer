32da5eb82a0c35d8f0a1e2bcff15f58b139b1b70
Ilya <selivanov.i@abcloudz.com>
2022-05-11 18:18:39 +0300

    create MainActivity.kt

---
+++ app/src/main/java/com/example/videoplayer/presentation/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.videoplayer.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.videoplayer.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
